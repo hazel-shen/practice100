2f95cb85d4c558ddf95974d8db48cb2e63be5a97
hazel-shen <hazel.shen@ui.com>
2023-09-05 00:33:11 +0800

    Rust - Combiner: Recording progress #15 replace te function
    Signed-off-by: Hazel Shen <mail@hazel.style>

--- Rust/combiner/src/main.rs
+++ Rust/combiner/src/main.rs
@@ -1,31 +1,57 @@
-// Lesson #6
+// Lesson #15
 fn main() {
+    let args = Args::new();
     println!("Hello, world!");
-  }
-  
-  fn get_nth_arg(n: usize) -> String {
+}
+
+#[derive(Debug)]
+struct Args {
+    image_1: String,
+}
+
+impl Args {
+    fn new() -> Self {
+        Args {
+            image_1: String::new(),
+        }
+    }
+}
+
+fn get_nth_arg(n: usize) -> String {
     std::env::args().nth(n).unwrap()
-  }
-  
-  #[cfg(test)]
-  mod tests {
-    use crate::get_nth_arg;
-  
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
     #[test]
-    fn get_nth_arg_takes_usize() {
-      let _ = get_nth_arg(0usize);
-      assert!(true, "Your get_nth_arg function should take a usize argument.");
+    #[should_panic]
+    fn args_struct_has_image_1_field() {
+        let new_arg = Args::new();
+        let my_arg = Args {
+            image_1: get_nth_arg(1),
+        };
+        assert!(new_arg.image_1 == my_arg.image_1);
     }
+
     #[test]
-    fn get_nth_arg_returns_string() {
-      let val = get_nth_arg(0);
-      assert!(reg_with_con(r"target/debug/deps", val));
+    fn image_1_defined_with_args() {
+        let file_contents = return_file_in_src("main.rs");
+        assert!(reg_with_con(r"image_1:\s*get_nth_arg\(1\)", file_contents));
     }
-  
+
     fn reg_with_con(regex: &str, file_contents: String) -> bool {
-      use regex::Regex;
-  
-      Regex::new(regex).unwrap().is_match(&file_contents)
+        use regex::Regex;
+
+        Regex::new(regex).unwrap().is_match(&file_contents)
     }
-  }
-  
+    fn return_file_in_src(filename: &str) -> String {
+        use std::fs::read_to_string;
+
+        match read_to_string(String::from("combiner/src/") + filename) {
+            Ok(file_contents) => file_contents,
+            Err(_) => String::from(""),
+        }
+    }
+}
