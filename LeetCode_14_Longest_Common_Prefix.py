300a12b0491631e23732660b1744a91bc49cf98b
Hazel Shen <hazel.shen@ui.com>
2025-06-10 17:07:19 +0800

    Create LeetCode_14_Longest_Common_Prefix.py
    Signed-off-by: Hazel Shen <mail@hazel.style>

---
+++ LeetCode_14_Longest_Common_Prefix.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+
+        shortest_string = strs[0]
+        
+        for i, word in enumerate(strs): # Find out the shortest word
+            if i > 0 and len(word) < len(shortest_string):
+                shortest_string = word
+
+        same_char = []
+        flag = True
+        last_index = 0
+        result = ""
+        for i in range(len(shortest_string)):        
+            for word in strs:
+                if word[i] != shortest_string[i]:
+                    flag = False
+                    break
+                else:
+                    last_index = i
+
+            if flag == False: # Stop comparing
+                break
+
+            if flag == True:
+                result = ''.join(shortest_string[:last_index + 1])
+
+        return result
+                
+            
+                 
+                
+        
