c1460759587812ab789e759607c3400c45b54bb6
Hazel <hazel@hazel.localdomain>
2025-06-18 15:57:20 +0800

    Create LeetCode 35 (BFS and DFS)

---
+++ LeetCode_35_Search_Insert_Position_(Traversal_Version_BFS).py
@@ -0,0 +1,28 @@
+from collections import deque
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        queue = deque([root]) # 初始化陣列
+        depth = 0 # 初始化深度
+        if root is None:
+            return depth
+        
+        while queue:
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left is not None:
+                    queue.append(node.left)
+                if node.right is not None:
+                    queue.append(node.right)
+            depth += 1
+        
+        return depth
